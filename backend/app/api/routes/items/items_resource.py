bfda6bcc72153e68f9f7e2c08724c920033e1aba
Marcel Tauber <marceltbr@gmail.com>
2022-11-23 16:25:35 +0000

    Fix backend bug with conditional variable

--- backend/app/api/routes/items/items_resource.py
+++ backend/app/api/routes/items/items_resource.py
@@ -68,6 +68,7 @@ async def create_new_item(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail=strings.ITEM_ALREADY_EXISTS,
         )
+    item_image = item_create.image if item_create.image else "/placeholder.png"  
     item = await items_repo.create_item(
         slug=slug,
         title=item_create.title,
@@ -75,7 +76,7 @@ async def create_new_item(
         body=item_create.body,
         seller=user,
         tags=item_create.tags,
-        image=item_create.image if item_create.image else "/placeholder.png",
+        image=item_image
     )
     send_event('item_created', {'item': item_create.title})
     return ItemInResponse(item=ItemForResponse.from_orm(item))
